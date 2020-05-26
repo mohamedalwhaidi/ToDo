c18ee935b451355bbffbf503f2f409300a6a005b
NiMo- <mo5-98@hotmail.com>
2020-05-26 16:32:02 +0300

    add firebase sitting

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home(),),);
+}
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
